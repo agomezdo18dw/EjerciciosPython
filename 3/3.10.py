a92c39bc93186672225cf779134ee8fd76e3e728
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2020-09-24 11:14:23 +0200

    deleted non used elif

--- 3/3.10.py
+++ 3/3.10.py
@@ -13,8 +13,6 @@ while True:
     num = int(input("Introduce un numero: "))
     if num != 0:
         numeros.append(num)
-    elif :
-
     else:
         print()
         print('------------------')
